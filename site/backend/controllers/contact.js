d6497c77900cecad610d713d9e543a78aeed468a
Habibou Moulaye Zeini <h.moulayezeini@students.ephec.be>
2023-06-05 17:28:15 +0200

    rajout de clé SSL pour la DB

--- site/backend/controllers/contact.js
+++ site/backend/controllers/contact.js
@@ -1,12 +1,18 @@
 
 const mysql = require("mysql");
 const nodemailer = require('nodemailer');
+const fs = require("fs");
 
 const pool = mysql.createPool({
     host: '34.79.25.79',
     user: 'serveur-Kabori',
     password: 'Pd\)T23nGB-Tc3RU',
     database: 'siteKabori',
+    ssl: {
+        ca: fs.readFileSync('/home/bitnami/certifs/server-ca.pem'), // Chemin vers le certificat de l'autorité de certification
+        key: fs.readFileSync('/home/bitnami/certifs/client-key.pem '), // Chemin vers la clé privée du client
+        cert: fs.readFileSync('/home/bitnami/certifs/client-cert.pem '), // Chemin vers le certificat du client
+    },
     connectionLimit: 20
 });
 exports.getContact= (req, res) => {
