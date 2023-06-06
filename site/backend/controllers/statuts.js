6604f947d683eb26b9802b1ebab931a80e03b907
Habibou Moulaye Zeini <h.moulayezeini@students.ephec.be>
2023-06-06 16:29:06 +0200

    Fix controllers

--- site/backend/controllers/statuts.js
+++ site/backend/controllers/statuts.js
@@ -1,16 +1,9 @@
 const mysql = require("mysql");
-<<<<<<< HEAD
 const fs = require("fs");
 const pool = mysql.createPool({
     host: '34.79.25.79',
     user: 'serveur-Kabori',
     password: 'Pd\\)T23nGB-Tc3RU',
-=======
-const pool = mysql.createPool({
-    host: '34.79.25.79',
-    user: 'root',
-    password: 'admin',
->>>>>>> 6f53895 (modification et ajout pour la gestion catalogue cotée admin et ajout de gestion de commande coté admin aussi.)
     database: 'siteKabori',
     connectionLimit: 10 // Nombre maximal de connexions dans la pool
 });
